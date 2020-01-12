aadec005305010bc023f9495025b707382cbd00b
Julia Nascimento <ju.araujo11@yahoo.com.br>
2020-01-12 12:10:03 -0300

    Transforma em float os dados que estavam como strings

--- main.py
+++ main.py
@@ -39,11 +39,26 @@ def lerArquivosMinucias(arquivo):
 
     return (valores_x, valores_y, angulo, tipo)
 
+# transforma os valores de string para float
+def transformaEmFloat(minutiae_parameter):
+    float_minutiae = []
+    for item in minutiae_parameter:
+        float_minutiae.append(float(item))
+    
+    return float_minutiae
 
 abreImagem(sys.argv[1])
 abreImagem(sys.argv[2])
 
 (valores_x_referencia, valores_y_referencia, angulo_referencia, tipo_referencia) = lerArquivosMinucias(NOME_ARQUIVO_MINUCIAS_REFERENCIA)
 
+valores_x_referencia = transformaEmFloat(valores_x_referencia)
+valores_y_referencia = transformaEmFloat(valores_y_referencia)
+angulo_referencia = transformaEmFloat(angulo_referencia)
+
 (valores_x_comparacao, valores_y_comparacao, angulo_comparacao, tipo_comparacao) = lerArquivosMinucias(NOME_ARQUIVO_MINUCIAS_COMPARACAO)
+
+valores_x_comparacao = transformaEmFloat(valores_x_comparacao)
+valores_y_comparacao = transformaEmFloat(valores_y_comparacao)
+angulo_comparacao = transformaEmFloat(angulo_comparacao)
 
