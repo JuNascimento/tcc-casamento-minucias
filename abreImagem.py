4cb5b5fcce4ec9cc621e666e52df47995bbbfbbc
Julia Nascimento <ju.araujo11@yahoo.com.br>
2020-01-14 09:51:14 -0300

    Substitui a biblioteca skimage pela matplotlib

--- abreImagem.py
+++ abreImagem.py
@@ -1,8 +1,12 @@
-import skimage
-from skimage import io
+import matplotlib
+import matplotlib.pyplot as plt
 
 def abreImagem(arquivo):
     caminho_arquivo = '/Users/julia.nascimento/Julia/uerj/10_periodo/tcc/tcc-casamento-minucias/imagens_minucias/' + arquivo
-    imagem = io.imread(caminho_arquivo)
-    io.imshow(imagem, cmap='gray') 
-    io.show()
+    imagem = plt.imread(caminho_arquivo)
+    print("Tamanho da imagem --> ", imagem.shape)
+    plt.title(arquivo)
+    plt.xlabel('eixo x')
+    plt.ylabel('eixo y')
+    plt.imshow(imagem, cmap='gray')
+    plt.show()
