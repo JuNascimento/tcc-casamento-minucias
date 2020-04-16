9ff5af9c25c7de2e3ea82a613d0e22dbc84250e6
Julia Nascimento <ju.araujo11@yahoo.com.br>
2020-04-15 22:01:27 -0300

    Isola em uma função a transformação de listas para matrizes numpy

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import sys
-import skimage
 import numpy as np
-from skimage import io
 
 from abreImagem import abreImagem
 from lerArquivosMinucias import lerArquivosMinucias
 from transformaEmFloat import transformaEmFloat
 from formaTuplas import formaTuplas
 from formaDeltas import formaDeltas
+from transformaMinuciasEmMatrizes import transformaMinuciasEmMatrizes
 
 NOME_ARQUIVO_MINUCIAS_REFERENCIA = sys.argv[1].split(".tif")[0] + ".txt"
 NOME_ARQUIVO_MINUCIAS_COMPARACAO = sys.argv[2].split(".tif")[0] + ".txt"
@@ -31,3 +30,11 @@ angulo_comparacao = transformaEmFloat(angulo_comparacao)
 print("Número de minúcias encontradas na imagem de referência --> ", len(valores_x_referencia))
 print("Número de minúcias encontradas na imagem de comparação --> ", len(valores_x_comparacao))
 print("\n--------------------------\n")
+
+valores_x_referencia = transformaMinuciasEmMatrizes(valores_x_referencia)
+valores_y_referencia = transformaMinuciasEmMatrizes(valores_y_referencia)
+angulo_referencia = transformaMinuciasEmMatrizes(angulo_referencia)
+
+valores_x_comparacao = transformaMinuciasEmMatrizes(valores_x_comparacao)
+valores_y_comparacao = transformaMinuciasEmMatrizes(valores_y_comparacao)
+angulo_comparacao = transformaMinuciasEmMatrizes(angulo_comparacao)
---
+++ transformaMinuciasEmMatrizes.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def transformaMinuciasEmMatrizes(minucias):
+    minucias_em_numpy = np.array(minucias)
+    quantidade_minucias = len(np.array(minucias))
+
+    valores = np.reshape(minucias_em_numpy, (quantidade_minucias, 1))
+
+    return valores
